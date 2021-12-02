7e05f1722f9205f7399486bf93181e1d29ea795c
trulytristam <49181012+trulytristam@users.noreply.github.com>
2021-12-02 00:55:16 -0500

    Update AdventOfCode2021 - DAY 1.py

--- AdventOfCode2021 - DAY 1.py
+++ AdventOfCode2021 - DAY 1.py
@@ -1,6 +1,8 @@
-a = [int(x) for x in code.split('\n')]
-last, inc = a[0]+a[1]+a[2], 0
-for i in range(3,len(a)):
-    c = last + a[i] - a[i-3]
-    inc = inc+1 if c>last else inc
-    last = c
+def answer(code):
+    a = [int(x) for x in code.split('\n')]
+    last, inc = a[0]+a[1]+a[2], 0
+    for i in range(3,len(a)):
+        c = last + a[i] - a[i-3]
+        inc = inc+1 if c>last else inc
+        last = c
+    return inc
